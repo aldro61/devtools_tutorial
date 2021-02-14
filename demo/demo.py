79c765f28c84daca168d097b8e9fa2636c50b9f8
Alexandre Drouin <alexandre.drouin1@gmail.com>
2021-02-13 22:37:25 -0500

    Demo tests

--- demo/demo.py
+++ demo/demo.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
 
+
+class DummyNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if len(x.shape) != 2:
+            raise ValueError("Shape must be 2 dim.")
+
+        return torch.sigmoid(x)
--- demo/test_demo.py
+++ demo/test_demo.py
@@ -1,1 +1,39 @@
 import pytest
+import torch
+
+from demo import DummyNet
+
+
+def test_init() -> None:
+    """
+    Test that the network can be initialized correctly
+
+    """
+    # As long as this doesn't fail, the test will pass.
+    # Note: This is a pedagogical example, we don't usually test this.
+    DummyNet()
+
+
+def test_forward_input_validation() -> None:
+    """
+    Test that input valiatin works in the forward pass
+
+    """
+    m = DummyNet()
+
+    with pytest.raises(ValueError):
+        m(torch.ones(10,))
+
+
+def test_forward_output() -> None:
+    """
+    Test that the forward pass outputs values in [0, 1]
+
+    """
+    m = DummyNet()
+
+    for _ in range(100):
+        inp = torch.rand(5, 10) * torch.randint(0, 1000, (1,))
+        out = m(inp)
+        assert (out > 1).sum() == 0
+        assert (out < 0).sum() == 0
